58f0b731d14844fa30be171cf9343e1230a9bea1
Jayda Schmidt <jaydaschmidt808@gmail.com>
2022-11-04 06:36:17 -0400

    Update Jenkinsfile

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -8,7 +8,7 @@ module.exports = function(app) {
 /*  app.use(proxy('/api', { target: 'http://172.19.0.2:8080/' }));
     app.use(proxy('/account', { target: 'http://172.19.0.3:8081/' }));  */
 
-    app.use(proxy('/api', { target: 'http://10.98.155.90:8080/' }));
-    app.use(proxy('/account', { target: 'http://10.98.184.21:8081/' }));  
+    app.use(proxy('/api', { target: 'http://10.98.97.41:8080/' }));
+    app.use(proxy('/account', { target: 'http://10.102.38.124:8081/' }));  
 
 };
